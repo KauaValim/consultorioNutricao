e71c90b6077ffc6cd74fc2d424d89b43a89cdae0
KauaValim <kaua_a.val@outlook.com>
2024-06-17 22:25:29 -0300

    Aula 17/06/2024
    Estilização da página

--- cadastro.php
+++ cadastro.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -20,18 +21,22 @@
     </header>
 
     <main class="content">
-        <form action="perfil.php" method="post">
+        <form action="cadastro.php" method="POST">
             <h2>Cadastro</h2>
+            <div class="box">
             <input type="text" name="nome" placeholder="Nome" required>
             <input type="number" name="anoNasc" placeholder="Ano Nascimento" required>
             <input type="number" name="peso" placeholder="Peso" step="0.01" required>
             <input type="number" name="altura" placeholder="Altura" step="0.01" required>
+            </div>
+            <div class="btn">
             <input type="submit" value="Cadastrar">
             <input type="reset" value="Limpar">
+            </div>
         </form>
         <?php
         require "./consultorio.php";
-
+        echo "<div class='resposta'><p>";
         $cliente = new consultorio();
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if (isset($_POST["nome"]) && isset($_POST["anoNasc"]) && ($_POST["anoNasc"] > 1900 && $_POST["anoNasc"] <= date("Y")) && isset($_POST["peso"]) && isset($_POST["altura"])) {
@@ -42,7 +47,7 @@
                 echo $cliente->__toString();
             }
         }
-        
+        echo "</p></div>";
         ?>
     </main>
 
